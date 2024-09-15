1d45516360b69df77ea2ad8e286c628733a2b05e
BlazingWarlord <66896016+BlazingWarlord@users.noreply.github.com>
2024-09-15 21:11:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,14 +56,17 @@ for message in st.session_state.messages:
 
 # React to user input
 if prompt := st.chat_input("You: "):
-    # Display user message in chat message container
-    st.chat_message("user").markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
-    response = bot_reply(prompt)
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        st.markdown(response)
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    if st.session_state['credits'] > 0:
+        # Display user message in chat message container
+        st.chat_message("user").markdown(prompt)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+    
+        response = bot_reply(prompt)
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            st.markdown(response)
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
+    else:
+        st.error('Looks like you are out of credits... ', icon="🚨")
